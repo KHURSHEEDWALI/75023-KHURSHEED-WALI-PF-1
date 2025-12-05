9285fe1cef34c10cfc975086583f10fdc3fdea16
KHURSHEEDWALI <75023@students.riphah.edu.pk>
2025-12-05 20:06:55 +0500

    Add files via upload

---
+++ riphah.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char word[] = "Riphah International University Islamabad";
+    cout << word << endl;
+    system ("pause");
+    return 0;
+}
